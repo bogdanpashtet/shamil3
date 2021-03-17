f49ee0fd907e32de6e3b71d4314c07a82bd067ed
bogdanpashtet <58053351+bogdanpashtet@users.noreply.github.com>
2021-03-17 12:37:01 +0300

    Add files via upload

---
+++ lab3_block2.3.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab3._2._3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите код:");
+            var start_code = Console.ReadLine();
+            Console.Write("Введите ключ:");
+            var key = Convert.ToInt32(Console.ReadLine(), 2);
+            var end_code=(Convert.ToInt32(start_code,2) ^ key);
+            start_code = Convert.ToString(end_code,2);
+            Console.WriteLine("Закодированный код: {0}", start_code);
+            end_code = Convert.ToInt32(start_code, 2) ^ key;
+            Console.WriteLine("Декодированный код: {0}", Convert.ToString(end_code, 2));
+        }
+
+    }
+}
